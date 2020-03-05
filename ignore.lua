ef6333e87cb5d6935a9a3c12a75f8282c0ccadba
R3R3R3 <>
2020-03-05 21:06:29 +0100

    /ignore, /unignore, /ignore_list (ignore list isn't persisted yet)

---
+++ ignore.lua
@@ -0,0 +1,77 @@
+
+local storage = minetest.get_mod_storage()
+
+civchat.player_ignores = {}
+
+minetest.register_chatcommand(
+   "ignore",
+   {
+      params = "<player>",
+      description = "Adds a name to the sender's chat ignore list.",
+      func = function(name, param)
+         if param == "" then
+            return false, "Please specify a player to ignore."
+         end
+
+         civchat.player_ignores[name] = civchat.player_ignores[name] or {}
+         civchat.player_ignores[name][param] = true
+
+         return true, "Player '" .. param .. "' was added to your ignore list."
+      end
+   }
+)
+
+minetest.register_chatcommand(
+   "unignore",
+   {
+      params = "<player>",
+      description = "Removes player from the sender's chat ignore list.",
+      func = function(name, param)
+         if param == "" then
+            return false, "Please specify a player to unignore."
+         end
+         civchat.player_ignores[name][param] = nil
+         return true, "Player '" .. param .. "' was removed from your ignore list."
+      end
+   }
+)
+
+local C = minetest.colorize
+
+local function table_keyvals(tab)
+   local keyset = {}
+   local valset = {}
+   local n = 0
+   for k, v in pairs(tab) do
+      n = n + 1
+      keyset[n] = k
+      valset[n] = v
+   end
+   return keyset, valset
+end
+
+minetest.register_chatcommand(
+   "ignore_list",
+   {
+      params = "",
+      description = "Returns the sender's full chat ignore list.",
+      func = function(name)
+         local ignores = civchat.player_ignores[name]
+         if ignores then
+            local list = table.concat(table_keyvals(ignores), "  ")
+            return true, C("#0f0", "Ignored Players: ") .. list
+         else
+            return true, "You are not currently ignoring anyone."
+         end
+      end
+   }
+)
+
+-- Disallow messages to go through if target has /ignore-d the source
+civchat.register_handler(function(from, to, msg)
+      local ignore_entries = civchat.player_ignores[to]
+      if ignore_entries then
+         return not ignore_entries[from]
+      end
+      return true
+end)
--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ dofile(modpath .. "api.lua")
 dofile(modpath .. "pm.lua")
 dofile(modpath .. "handlers.lua")
 dofile(modpath .. "chat.lua")
+dofile(modpath .. "ignore.lua")
 
 minetest.log("[CivChat] Loaded CivChat.")
 
